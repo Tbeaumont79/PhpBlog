319da2730aed3409351ffb2162be1e3d0f25f42a
tbeaumont <tbeaumont79@icloud.com>
2019-10-11 18:49:48 +0200

    fixing some probleme with the controller !

--- Ctrl/Ctrl.php
+++ Ctrl/Ctrl.php
@@ -1,11 +1,17 @@
 <?php
+
   function Ctrl()
   {
     switch ($_GET['page']) {
       case 'sign_in':
         $ctrl .= signInAndSignUpForm();
         break;
-
+      case 'sign_up':
+        $ctrl .= signInAndSignUpForm();
+        break;
+      case 'index':
+        $ctrl .= HomeView();
+        break;
       default:
         $ctrl .= HomeView();
         break;
