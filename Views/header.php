78cd7ff0dd84fafb2ff8118f19e2d486444ccd35
tbeaumont <tbeaumont79@icloud.com>
2019-10-15 00:10:04 +0200

    checking the return of session start

--- Views/header.php
+++ Views/header.php
@@ -2,8 +2,9 @@
 
   function headerPage()
   {
+    debug(session_start());
     debug($_SESSION);
-    $htmlElement .= '<?php session_start(); ?><html>';
+    $htmlElement .= '<html>';
     $htmlElement .= '<head>';
     $htmlElement .=   '<meta charset="utf-8">';
     $htmlElement .=   '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
