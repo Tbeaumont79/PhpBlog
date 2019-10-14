cc232085ecca415fb1e2064651bf6a868f550370
tbeaumont <tbeaumont79@icloud.com>
2019-10-15 00:33:34 +0200

    changing the unset session

--- Model/logout.php
+++ Model/logout.php
@@ -4,7 +4,7 @@
     if (isset($_SESSION['username'])) {
       $msg .= success("you have been succesfully disconnected !");
     }
-    session_unset($_SESSION['username']);
+    session_unset($_SESSION);
     session_destroy();
     return $msg.signInAndSignUpForm();
   }
