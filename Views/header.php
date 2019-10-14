bee2277166660986ef5d89b8b3f8b5e446945fdd
tbeaumont <tbeaumont79@icloud.com>
2019-10-15 00:08:07 +0200

    try a little tricks

--- Views/header.php
+++ Views/header.php
@@ -2,9 +2,8 @@
 
   function headerPage()
   {
-    session_start();
     debug($_SESSION);
-    $htmlElement .= '<html>';
+    $htmlElement .= '<?php session_start(); ?><html>';
     $htmlElement .= '<head>';
     $htmlElement .=   '<meta charset="utf-8">';
     $htmlElement .=   '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">';
