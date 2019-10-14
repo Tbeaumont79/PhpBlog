97b05c942f3b70d99a9a416f7df483d1a9abd047
tbeaumont <tbeaumont79@icloud.com>
2019-10-15 00:34:34 +0200

    alter

--- Model/logout.php
+++ Model/logout.php
@@ -4,8 +4,8 @@
     if (isset($_SESSION['username'])) {
       $msg .= success("you have been succesfully disconnected !");
     }
-    session_unset($_SESSION);
     session_destroy();
+    session_unset($_SESSION);
     return $msg.signInAndSignUpForm();
   }
 
